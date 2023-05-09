cd2187f2918e386c9b999083ec0f5999501f0468
Yonatan Sasson <yonatanxd7@gmail.com>
2023-05-09 05:40:34 +0300

    changed to reconcile from annotation

--- main.go
+++ main.go
@@ -7,41 +7,32 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 	"time"
 
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
+	appsv1 "k8s.io/api/apps/v1"
+	_ "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/metrics/pkg/client/clientset/versioned"
-
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 )
 
 var (
 	kubeconfigPath   string
-	sourceNamespace  string
-	targetNamespace  string
-	cpuThreshold     int64
 	reconcileTimeout time.Duration
 )
 
 func init() {
 	flag.StringVar(&kubeconfigPath, "kubeconfig", "", "Path to the kubeconfig file")
-	flag.StringVar(&sourceNamespace, "source-namespace", "", "Source namespace")
-	flag.StringVar(&targetNamespace, "target-namespace", "", "Target namespace")
-	flag.Int64Var(&cpuThreshold, "cpu-threshold", 90, "CPU threshold percentage")
 	flag.DurationVar(&reconcileTimeout, "reconcile-timeout", 5*time.Minute, "Reconciliation timeout duration")
 }
 
 func main() {
 	flag.Parse()
 
-	if kubeconfigPath == "" || sourceNamespace == "" || targetNamespace == "" {
-		log.Fatal("kubeconfig, source-namespace, and target-namespace flags are required")
+	if kubeconfigPath == "" {
+		log.Fatal("kubeconfig flag is required")
 	}
 
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
@@ -52,11 +43,6 @@ func main() {
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		log.Fatalf("Failed to create clientset: %v", err)
-	}
-
-	metricsClient, err := versioned.NewForConfig(config)
-	if err != nil {
-		log.Fatalf("Failed to create metrics client: %v", err)
 	}
 
 	stopCh := make(chan os.Signal, 1)
@@ -70,164 +56,114 @@ func main() {
 		cancel()
 	}()
 
-	err = reconcileDeployment(ctx, clientset, metricsClient)
-	if err != nil {
-		log.Fatalf("Failed to reconcile Deployment: %v", err)
-	}
-
-	log.Println("Reconciliation completed!")
-}
-
-func reconcileDeployment(ctx context.Context, clientset *kubernetes.Clientset, metricsClient *versioned.Clientset) error {
-	deploymentsClient := clientset.AppsV1().Deployments(sourceNamespace)
-	deploymentName := "example-deployment"
-
-	deployment, err := deploymentsClient.Get(ctx, deploymentName, v1.GetOptions{})
-	if err != nil {
-		if errors.IsNotFound(err) {
-			return fmt.Errorf("Deployment %q not found", deploymentName)
-		}
-		return err
-	}
-
-	log.Printf("Reconciling Deployment %q\n", deploymentName)
-
-	podList, err := clientset.CoreV1().Pods(sourceNamespace).List(ctx, v1.ListOptions{LabelSelector: labelsToSelector(deployment.Spec.Selector)})
-	if err != nil {
-		return fmt.Errorf("Failed to retrieve Pods for Deployment %q: %v", deploymentName, err)
-	}
-
-	cpuUsagePercent, err := getCPUUsagePercent(ctx, metricsClient, deployment.Namespace, podList)
-	if err != nil {
-		return fmt.Errorf("Failed to retrieve CPU usage for Deployment %q: %v", deploymentName, err)
-	}
-
-	if cpuUsagePercent > cpuThreshold {
-		err = moveDeployment(ctx, clientset)
-		if err != nil {
-			return fmt.Errorf("Failed to move Deployment %q: %v", deploymentName, err)
-		}
-
-		err = moveService(ctx, clientset)
-		if err != nil {
-			return fmt.Errorf("Failed to move Service for Deployment %q: %v", deploymentName, err)
-		}
-
-		log.Printf("Deployment %q and Service moved to %q\n", deploymentName, targetNamespace)
-	} else {
-		log.Printf("Deployment %q CPU usage (%v%%) is below the threshold (%v%%)\n", deploymentName, cpuUsagePercent, cpuThreshold)
-	}
-
-	return nil
-}
-
-func getCPUUsagePercent(ctx context.Context, metricsClient *versioned.Clientset, namespace string, podList *corev1.PodList) (int64, error) {
-	totalCpuUsage := int64(0)
-	totalCpuLimit := int64(0)
-
-	for _, pod := range podList.Items {
-		podMetrics, err := metricsClient.MetricsV1beta1().PodMetricses(namespace).Get(ctx, pod.Name, v1.GetOptions{})
-		if err != nil {
-			return 0, fmt.Errorf("Failed to retrieve metrics for Pod %q: %v", pod.Name, err)
-		}
-
-		for _, container := range podMetrics.Containers {
-			cpuUsage := container.Usage[corev1.ResourceCPU]
-			cpuLimit := container.Usage[corev1.ResourceLimitsCPU]
-
-			cpuUsageMillis := cpuUsage.MilliValue()
-			cpuLimitMillis := cpuLimit.MilliValue()
-
-			totalCpuUsage += cpuUsageMillis
-			totalCpuLimit += cpuLimitMillis
-		}
-	}
-
-	if totalCpuLimit == 0 {
-		return 0, nil
-	}
-
-	cpuUsagePercent := (totalCpuUsage * 100) / totalCpuLimit
-	return cpuUsagePercent, nil
-}
-
-func labelsToSelector(labels *v1.LabelSelector) string {
-	var labelSelectors []string
-	for key, value := range labels.MatchLabels {
-		labelSelectors = append(labelSelectors, fmt.Sprintf("%s=%s", key, value))
-	}
-	return strings.Join(labelSelectors, ",")
-}
-
-func moveDeployment(ctx context.Context, clientset *kubernetes.Clientset) error {
-	deploymentsClient := clientset.AppsV1().Deployments(sourceNamespace)
-	deploymentName := "example-deployment"
-
-	deployment, err := deploymentsClient.Get(ctx, deploymentName, v1.GetOptions{})
-	if err != nil {
-		if errors.IsNotFound(err) {
-			return fmt.Errorf("Deployment %q not found", deploymentName)
-		}
-		return err
-	}
-
-	deployment.Namespace = targetNamespace
-	deployment.ResourceVersion = ""
-	_, err = deploymentsClient.Create(ctx, deployment, v1.CreateOptions{})
-	if err != nil {
-		if errors.IsAlreadyExists(err) {
-			_, err = deploymentsClient.Update(ctx, deployment, v1.UpdateOptions{})
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("Reconciliation stopped")
+			return
+		case <-time.After(reconcileTimeout):
+			log.Println("Reconciliation timeout reached")
+			return
+		default:
+			err := reconcileDeployment(ctx, clientset)
 			if err != nil {
-				return err
+				log.Printf("Failed to reconcile Deployment: %v\n", err)
 			}
-		} else {
-			return err
+			time.Sleep(10 * time.Second)
+		}
+	}
+}
+
+func reconcileDeployment(ctx context.Context, clientset *kubernetes.Clientset) error {
+	deploymentsClient := clientset.AppsV1().Deployments(v1.NamespaceAll)
+	selector := labels.Set{"app": "example-app"}.AsSelector()
+
+	deploymentList, err := deploymentsClient.List(ctx, v1.ListOptions{LabelSelector: selector.String()})
+	if err != nil {
+		return fmt.Errorf("Failed to retrieve Deployments: %v", err)
+	}
+
+	for _, deployment := range deploymentList.Items {
+		annotations := deployment.GetAnnotations()
+
+		if annotations["ns"] == "test" {
+			err = moveDeployment(ctx, clientset, &deployment, "test2")
+			if err != nil {
+				log.Printf("Failed to move Deployment %q to test2: %v\n", deployment.Name, err)
+			}
 		}
 	}
 
-	deletePropagation := v1.DeletePropagationForeground
-	deleteOptions := &v1.DeleteOptions{PropagationPolicy: &deletePropagation}
-	err = deploymentsClient.Delete(ctx, deploymentName, *deleteOptions)
-	if err != nil {
-		return err
-	}
-
-	log.Printf("Deployment %q moved from %q to %q\n", deploymentName, sourceNamespace, targetNamespace)
 	return nil
 }
 
-func moveService(ctx context.Context, clientset *kubernetes.Clientset) error {
-	servicesClient := clientset.CoreV1().Services(sourceNamespace)
-	serviceName := "example-service"
+func moveDeployment(ctx context.Context, clientset *kubernetes.Clientset, deployment *appsv1.Deployment, targetNamespace string) error {
+	sourceNamespace := deployment.Namespace
+	deploymentName := deployment.Name
 
-	service, err := servicesClient.Get(ctx, serviceName, v1.GetOptions{})
+	log.Printf("Moving Deployment %q from %q to %q\n", deploymentName, sourceNamespace, targetNamespace)
+
+	// Update the deployment's namespace
+	deployment.Namespace = targetNamespace
+	_, err := clientset.AppsV1().Deployments(sourceNamespace).Update(ctx, deployment, v1.UpdateOptions{})
 	if err != nil {
-		if errors.IsNotFound(err) {
-			return fmt.Errorf("Service %q not found", serviceName)
-		}
-		return err
+		return fmt.Errorf("Failed to update Deployment %q namespace: %v", deploymentName, err)
+	}
+
+	// Create a new deployment in the target namespace
+	deployment.ResourceVersion = ""
+	deployment.UID = ""
+	_, err = clientset.AppsV1().Deployments(targetNamespace).Create(ctx, deployment, v1.CreateOptions{})
+	if err != nil {
+		return fmt.Errorf("Failed to create Deployment %q in namespace %q: %v", deploymentName, targetNamespace, err)
+	}
+
+	// Delete the original deployment in the source namespace
+	err = clientset.AppsV1().Deployments(sourceNamespace).Delete(ctx, deploymentName, v1.DeleteOptions{})
+	if err != nil {
+		return fmt.Errorf("Failed to delete Deployment %q from namespace %q: %v", deploymentName, sourceNamespace, err)
+	}
+
+	// Update the service's namespace
+	err = moveService(ctx, clientset, deployment, targetNamespace)
+	if err != nil {
+		return fmt.Errorf("Failed to move Service associated with Deployment %q: %v", deploymentName, err)
+	}
+
+	return nil
+}
+
+func moveService(ctx context.Context, clientset *kubernetes.Clientset, deployment *appsv1.Deployment, targetNamespace string) error {
+	sourceNamespace := deployment.Namespace
+	serviceName := deployment.Name
+
+	log.Printf("Moving Service %q from %q to %q\n", serviceName, sourceNamespace, targetNamespace)
+
+	// Update the service's namespace
+	service, err := clientset.CoreV1().Services(sourceNamespace).Get(ctx, serviceName, v1.GetOptions{})
+	if err != nil {
+		return fmt.Errorf("Failed to retrieve Service %q in namespace %q: %v", serviceName, sourceNamespace, err)
 	}
 
 	service.Namespace = targetNamespace
+	_, err = clientset.CoreV1().Services(sourceNamespace).Update(ctx, service, v1.UpdateOptions{})
+	if err != nil {
+		return fmt.Errorf("Failed to update Service %q namespace: %v", serviceName, err)
+	}
+
+	// Create a new service in the target namespace
 	service.ResourceVersion = ""
-	_, err = servicesClient.Create(ctx, service, v1.CreateOptions{})
+	service.UID = ""
+	_, err = clientset.CoreV1().Services(targetNamespace).Create(ctx, service, v1.CreateOptions{})
 	if err != nil {
-		if errors.IsAlreadyExists(err) {
-			_, err = servicesClient.Update(ctx, service, v1.UpdateOptions{})
-			if err != nil {
-				return err
-			}
-		} else {
-			return err
-		}
+		return fmt.Errorf("Failed to create Service %q in namespace %q: %v", serviceName, targetNamespace, err)
 	}
 
-	deleteOptions := &v1.DeleteOptions{}
-	err = servicesClient.Delete(ctx, serviceName, *deleteOptions)
+	// Delete the original service in the source namespace
+	err = clientset.CoreV1().Services(sourceNamespace).Delete(ctx, serviceName, v1.DeleteOptions{})
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed to delete Service %q from namespace %q: %v", serviceName, sourceNamespace, err)
 	}
 
-	log.Printf("Service %q moved from %q to %q\n", serviceName, sourceNamespace, targetNamespace)
 	return nil
 }
